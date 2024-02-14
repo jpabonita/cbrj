8e2a377f51c729c2c60e86731eb9626e4ed1fd87
jpabonita <159973708+jpabonita@users.noreply.github.com>
2024-02-13 22:18:43 -0400

    Initial commit

---
+++ database/migrations/2024_02_14_011549_create_people_tale.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people_tale', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->date('birth_date');
+            $table->string('gender');
+            $table->string('civil_status');
+            $table->string('email');
+            $table->string('contact_no');
+            $table->string('sitio');
+            $table->string('barangay');
+            $table->integer('address_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people_tale');
+    }
+};
